7f363b6e882c15d590a3a780962eac9cb4a036cc
guisbonddev <wguisbond@gmail.com>
2018-09-06 21:06:17 -0400

    recreated basic home view to include navs; created nav partials and imported the Bootstrap library

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,31 @@
+
+  <div class="col-md-2">
+      <br>
+    <center>
+      <img src="https://www.vatusa.net/img/logo-light.png" width="100%">
+        <br><br>
+      <h4>Virtual ARTCC OIS System</h4>
+        <hr>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">NAS Status</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">West ARTCC Directory</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">East ARTCC Directory</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Doumentation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">VATUSA</a>
+        </li>
+      </ul><hr>
+
+      <nav class="navbar navbar-transparent">
+        <p> &copy; VATUSA <br> Code by Will Guisbond and Blake Nahin </p>
+      </nav>
+    </center>
+  </div>
